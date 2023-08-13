423f4dd44f672923e90e97b67a21bde7ab4d90b2
Juliana Marroncelli <107325916+julianamarroncelli@users.noreply.github.com>
2023-08-13 00:24:47 -0300

    Update index.jsx

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Container } from "./style";
+import imgBanner from '../../assets/images/imageHome.png'
 
 export function Banner (){
   return(
     <Container>
-      <img src="\src\assets\images\imageHome.png"/>
-      
+      <img src={imgBanner} />
       <div className="title">
         <h1> Sabores inigualáveis </h1>
         <p> Sinta o cuidado do preparo com ingredientes selecionados </p>  
